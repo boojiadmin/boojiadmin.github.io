3152c9b0fe6ed3a5579d90e2e49493cc0dd68347
abdulkhaled <abdrkhaled@gmail.com>
2021-11-12 00:12:13 +0000

    add redirects for verify and change pass for apps

---
+++ changepassword.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+  console.log("web");
+  window.location = "www.booji.app/changepasswordWeb.html";
+}, 25);
+
+// If "custom-uri://" is registered the app will launch immediately and your
+// timer won't fire. If it's not set, you'll get an ugly "Cannot Open Page"
+// dialogue prior to the App Store application launching
+window.location = "booji://"+window.location.href;
---
+++ verify.js
@@ -0,0 +1,10 @@
+var initialUrl = window.location.href;
+setTimeout(function() {
+  console.log("web");
+  window.location = initialUrl;
+}, 25);
+
+// If "custom-uri://" is registered the app will launch immediately and your
+// timer won't fire. If it's not set, you'll get an ugly "Cannot Open Page"
+// dialogue prior to the App Store application launching
+window.location = "booji://"+window.location.href;
