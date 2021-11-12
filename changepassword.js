82a06250ed9df5f5febca3e2b93c3d62f5cce019
abdulkhaled <abdrkhaled@gmail.com>
2021-11-12 02:01:55 +0000

    fix redirect

--- changepassword.js
+++ changepassword.js
@@ -1,9 +1,11 @@
 setTimeout(function() {
   console.log("web");
-  window.location.href.replace("https://booji.app/changepasswordWeb.html");
-  window.location.href = window.location.href;
+  window.location.href =changepasswordWeb();
 }, 25);
 
+function changepasswordWeb() {
+    return( window.location.href.replace("https://booji.app/changepasswordWeb.html"));
+}
 // If "custom-uri://" is registered the app will launch immediately and your
 // timer won't fire. If it's not set, you'll get an ugly "Cannot Open Page"
 // dialogue prior to the App Store application launching
--- verify.js
+++ verify.js
@@ -1,9 +1,12 @@
 var initialUrl = window.location.href;
 setTimeout(function() {
   console.log("web");
-  window.location.href.replace("https://api.booji.app"+window.location.pathname+window.location.search);
-  window.location.href = window.location.href;
+  window.location.href = verifyApi();
 }, 25);
+
+function verifyApi() {
+    return(   window.location.href.replace("https://api.booji.app"+window.location.pathname+window.location.search));
+}
 
 // If "custom-uri://" is registered the app will launch immediately and your
 // timer won't fire. If it's not set, you'll get an ugly "Cannot Open Page"
